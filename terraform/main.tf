4291cbc9a5aac1c4980482b0323d754b1d0875b7
Tub <colorpulse@gmail.com>
2025-03-15 06:52:28 +0100

    duplicate outputs

--- terraform/main.tf
+++ terraform/main.tf
@@ -179,12 +179,3 @@ resource "aws_s3_bucket" "deployment" {
   bucket = "${local.resource_prefix}-deployment"
   tags   = local.tags
 }
-
-# Output the instance public IP
-output "app_public_ip" {
-  value = aws_instance.app.public_ip
-}
-
-output "s3_deployment_bucket" {
-  value = aws_s3_bucket.deployment.bucket
-}
